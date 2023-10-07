0f092d4abdb5edecdf01be2560fc6270300122db
bhavana ahire <bhavanaahire8601@gmail.com>
2023-10-07 21:09:22 +0530

    check Test2

--- src/test/java/demo/MultipleTestCase.java
+++ src/test/java/demo/MultipleTestCase.java
@@ -13,14 +13,7 @@ public class MultipleTestCase {
         driver.close();
 	driver.close();
     }
-    @Test
-    public void testcase2(){
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://flipkart.com/");
-        driver.manage().window().maximize();
-        driver.close();
-    }
+    
     @Test
     public void testcase4(){
         System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
